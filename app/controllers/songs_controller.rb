f168ca48de64559132687331eb882bfefa58426d
Chris Cardle <cdcardle@gmail.com>
2018-07-24 22:11:21 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,7 @@ class SongsController < ApplicationController
   def index
     if params[:artist_id]
       artist = Artist.find(params[:artist_id])
-      if artist.valid?
+      if artist
         @songs = artist.songs
       else
         redirect 'artists'
