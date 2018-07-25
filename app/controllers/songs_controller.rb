b6e329b86c646d13bbaf265f4598bad5fa935867
Chris Cardle <cdcardle@gmail.com>
2018-07-25 17:08:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,6 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-
       if Artist.find(params[:artist_id])
         @songs = Artist.find(params[:artist_id]).songs
       else
